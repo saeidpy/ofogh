396004aee3e9b3cfd9abd81009a4d08e9fbe01d1
saeepdy <saeedpy@gmail.com>
2021-08-03 19:10:43 +0430

    fix update at sorty

--- src/Api/Ad.js
+++ src/Api/Ad.js
@@ -24,7 +24,7 @@ const readAdApi = async ({ id }) => {
 const readAllAdApi = async () => {
   const { data } = await apiMiddleware({
     method: "GET",
-    url: "/ads?_sort=updateAt&_order=asc",
+    url: "/ads?_sort=updateAt&_order=desc",
     withAuth: true,
   });
   return data;
